acf08473728df0fc3bf8b33a03bbc46dc5bb05ee
caolifeng <caolifeng@agree.com.cn>
2024-01-23 18:32:28 +0800

    feat:提交接入dddeditor

--- example/src/superDocPlugins/block/index.js
+++ example/src/superDocPlugins/block/index.js
@@ -1,5 +1,6 @@
 import { Plugin } from '../../../../packages/api/dist/api.esm-bundler.js';
 import _Auae from '../../components/auae/index.vue';
+// import _Auae from '../../components/auae/testMount.vue';
 
 export class Auae extends Plugin.BlockBase {
   config = null;
